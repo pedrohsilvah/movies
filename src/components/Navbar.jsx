81d7f93ef229b49c822067fe94e806596f3a085f
Pedro <pedrohsilvasant@gmail.com>
2024-04-13 18:14:05 -0300

    Configurando api e criando conteúdo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+
+const Navbar = () => {
+    return (
+        <nav id='navbar'>
+            <h2>
+                <Link to="/">
+                    <BiCameraMovie />Movies
+                </Link>
+            </h2>
+            <form>
+                <input type="text"  placeholder="Busque um filmer" />
+                <button type='submit'>
+                    <BiSearchAlt2 />
+                </button>
+            </form>
+        </nav>
+    )
+}
+
+export default Navbar
